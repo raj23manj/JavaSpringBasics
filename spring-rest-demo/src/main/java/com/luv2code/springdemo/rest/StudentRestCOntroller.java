4ade05db285f43c15a177d844bf8f7a02ff10ceb
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-22 16:02:56 +0530

    Using @PathVariables for Rest

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestCOntroller.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestCOntroller.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +15,27 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestCOntroller {
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		
-		List<Student> theStudents = new ArrayList<>();
+	
+	private List<Student> theStudents;
+	
+	// using beans @PostConstruct to load data once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rasi"));
 		theStudents.add(new Student("Mary", "Smith"));
-		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return theStudents;
 	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return theStudents.get(studentId);
+	}
+	
+	
 }
